9cc5a1cb666370a14359ec40bd0635054af42c3e
Maxim <mksmsmirnov@gmail.com>
2021-07-14 20:58:04 +0300

    Initial commit

---
+++ src/main/java/ru/max/restSMA/RestSmaApplication.java
@@ -0,0 +1,13 @@
+package ru.max.restSMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/max/restSMA/RestSmaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.max.restSMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
